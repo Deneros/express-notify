dcc7ad06e82c4e12eaaffbd38ac73c36e816bea8
Nicolas <nicolas.velez99@hotmail.com>
2023-05-12 18:02:04 -0500

    [feat]:servicio de recordatorio

--- src/services/reminder.js
+++ src/services/reminder.js
@@ -1,19 +1,58 @@
 const cron = require('node-cron');
 const mailer = require('./mailer');
-const db = require('../config/db');
+const documentController = require('../controllers/DocumentController');
+
 
 function start() {
-  // Configurar el cron job para que se ejecute cada día a las 9:00 AM.
-  cron.schedule('42 9 * * *', async function() {
-    console.log('hola soy cron job')
-    // Realizar la consulta a la base de datos para obtener los documentos no firmados.
-    // const unsignedDocuments = await db.query('SELECT * FROM documents WHERE signed = false');
-
-    // for (let document of unsignedDocuments) {
-    //   // Enviar un correo electrónico para cada documento no firmado.
-    //   await mailer.sendMail(document.userEmail, 'Recordatorio de documento no firmado', `Por favor, firma el documento: ${document.name}`);
-    // }
+  cron.schedule('*/1 * * * *', function() {
+    documentController.getPendingDocuments(function(documents) {
+      let current_time = new Date().getTime();
+      for (let document of documents) {
+        // Si la cantidad de recordatorios es menor a 3, procedemos
+        if (document.cantidad_recordatorios < 3) {
+          // Verificamos que el último recordatorio no sea null
+          if (document.ultimo_recordatorio !== null) {
+            // Si ha pasado más de un día desde el último recordatorio, enviamos otro
+            if (((current_time - new Date(document.ultimo_recordatorio).getTime()) / (1000 * 60 * 60)) >= 24) {
+              // Código para enviar correo electrónico
+              documentController.updateDocumentAfterReminder(document.doc_id, function(result) {
+                console.log(result);
+              });
+            }
+          } else {
+            // Si el último recordatorio es null, asumimos que no se ha enviado ninguno y enviamos el primero
+            // Código para enviar correo electrónico
+            documentController.updateDocumentAfterReminder(document.doc_id, function(result) {
+              console.log(result);
+            });
+          }
+        }
+      }
+    });
   });
 }
 
-module.exports = { start };
+function test(){
+  documentController.getPendingDocuments(function (results) {
+    console.log(results); // Este código se ejecutará cuando se llame al callback
+});
+}
+
+
+module.exports = { start, test }
+
+
+          // let mailOptions = {
+          //   from: '"Firmadoc Notification" <your-email@example.com>',
+          //   to: document.det_cordes,
+          //   subject: 'Recordatorio de documento no firmado',
+          //   text: `Por favor, firma el documento: ${document.doc_nombre}`,
+          // };
+
+          // mailer.sendMail(mailOptions, (error, info) => {
+          //   if (error) {
+          //     console.error(error);
+          //   } else {
+          //     // Actualizar el documento después de enviar el correo electrónico
+          //   }
+          // });
