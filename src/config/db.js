ffaa9ab04eaf4bfb2cf9bfd8c34ab762cbde8eba
Nicolas <nicolas.velez99@hotmail.com>
2023-05-12 18:01:13 -0500

    [config]: conexion a la bd

--- src/config/db.js
+++ src/config/db.js
@@ -1,26 +1,15 @@
-var mysql = require('mysql');
+const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
-
-pool.getConnection(function(err, connection) {
-  if (err) {
-    console.error('Error connecting: ' + err.stack);
-    return;
-  }
-
-  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    connection.release(); // libera la conexión cuando hayas terminado
-
-    if (err) {
-      console.error('Error executing query: ' + err.stack);
-      return;
-    }
-
-    console.log('The solution is: ', rows[0].solution);
-  });
+const connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE
 });
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log("Conexión a la base de datos establecida.");
+});
+
+module.exports = connection;
