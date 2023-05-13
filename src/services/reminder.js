1da02bcb8dc3dc0d8ef817fd6ff8a32bb020b7b7
Acris <nicolas.velez99@hotmail.com>
2023-05-13 12:02:46 -0500

    [refactor]: se hace mas legible el codigo

--- src/services/reminder.js
+++ src/services/reminder.js
@@ -4,27 +4,21 @@ const documentController = require('../controllers/DocumentController');
 
 
 function start() {
-  cron.schedule('*/1 * * * *', function() {
-    documentController.getPendingDocuments(function(documents) {
+  cron.schedule('*/1 * * * *', function () {
+    documentController.getPendingDocuments(function (documents) {
       let current_time = new Date().getTime();
       for (let document of documents) {
-        // Si la cantidad de recordatorios es menor a 3, procedemos
+
         if (document.cantidad_recordatorios < 3) {
-          // Verificamos que el último recordatorio no sea null
+
           if (document.ultimo_recordatorio !== null) {
-            // Si ha pasado más de un día desde el último recordatorio, enviamos otro
+
             if (((current_time - new Date(document.ultimo_recordatorio).getTime()) / (1000 * 60 * 60)) >= 24) {
-              // Código para enviar correo electrónico
-              documentController.updateDocumentAfterReminder(document.doc_id, function(result) {
-                console.log(result);
-              });
+              sendReminderEmail(document);
             }
           } else {
-            // Si el último recordatorio es null, asumimos que no se ha enviado ninguno y enviamos el primero
-            // Código para enviar correo electrónico
-            documentController.updateDocumentAfterReminder(document.doc_id, function(result) {
-              console.log(result);
-            });
+
+            sendReminderEmail(document);
           }
         }
       }
@@ -32,12 +26,33 @@ function start() {
   });
 }
 
-function test(){
-  documentController.getPendingDocuments(function (results) {
-    console.log(results); // Este código se ejecutará cuando se llame al callback
-});
+console.log(mailer);
+
+function sendReminderEmail(document) {
+  let mailOptions = {
+    from: '"Firmadoc Notification" <notificaciones@firmadoc.co>',
+    to: document.det_cordes,
+    subject: 'Recordatorio de documento no firmado',
+    text: `Por favor, firma el documento: ${document.doc_nombre}`,
+  };
+
+  mailer.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+    } else {
+
+      documentController.updateDocumentAfterReminder(document.doc_id, function (result) {
+        console.log(result);
+      });
+    }
+  });
 }
 
+function test() {
+  documentController.getPendingDocuments(function (results) {
+    console.log(results); // Este código se ejecutará cuando se llame al callback
+  });
+}
 
 module.exports = { start, test }
 
