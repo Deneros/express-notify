fd0fd9dcbd41b5c845eeeece40824676f87fa83f
Nicolas <nicolas.velez99@hotmail.com>
2023-06-01 16:26:09 -0500

    [style]

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ const reminderService = require('./services/reminder');
 const app = express();
 const port =  3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!');
-});
+// app.get('/', (req, res) => {
+//   res.send('¡Hola Mundo!');
+// });
 
 reminderService.start();
 
